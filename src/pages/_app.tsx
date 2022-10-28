d05806761ca78faadd427bfe783aa58692d9440a
hy57in <gwsyl22@cau.ac.kr>
2022-10-28 12:00:35 +0900

    feat: pages 디렉토리 추가

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import theme from '@styles/theme';
+import GlobalStyle from '@styles/globalStyles';
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="엘라의 항공사 웹사이트의 컴포넌트 접근성 높이기 미션" />
+      <title>Homebrary</title>
+    </Head>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+);
+
+export default App;
