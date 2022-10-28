a7d7e2e397e45b72a4de1d2d2011e09f695357be
hy57in <gwsyl22@cau.ac.kr>
2022-10-28 11:42:42 +0900

    feat: globalStyle, theme 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function App() {
-  return (
-    <div>
-      캡스톤 도서관
-    </div>
-  );
+  return <Div>캡스톤 도서관</Div>;
 }
 
 export default App;
+
+const Div = styled.div`
+  background-color: red;
+`;
