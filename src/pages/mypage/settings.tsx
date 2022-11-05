2ac1a06e65f892eb211a8b7520156978d91b22b4
hy57in <gwsyl22@cau.ac.kr>
2022-11-06 00:27:17 +0900

    feat: mypage 설정 페이지 구현

---
+++ src/pages/mypage/settings.tsx
@@ -0,0 +1,68 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+import { requestError } from '@@types/shared';
+import Header from '@components/Header';
+import { NETWORK_ERROR, UNKNOWN_ERROR } from '@constants/error';
+import { showAlertModal } from '@utils/modal';
+
+const Settings = () => {
+  const handleLogout = async () => {
+    try {
+      // await requestLogout();
+      showAlertModal('로그아웃 되었습니다.');
+      location.href = '/';
+    } catch (error) {
+      const { response } = error as requestError;
+      if (!response) return showAlertModal(NETWORK_ERROR);
+
+      return showAlertModal(UNKNOWN_ERROR);
+    }
+  };
+
+  return (
+    <>
+      <Header title="개인정보 설정" />
+      <SettingSection onClick={handleLogout} className="setting-link">
+        로그아웃
+      </SettingSection>
+      <SettingSection>
+        <Link href="/mypage/signout" passHref className="setting-link">
+          회원 탈퇴
+        </Link>
+      </SettingSection>
+      <SettingSection>
+        <Link href="/mypage/changeNickName" passHref className="setting-link">
+          닉네임 변경
+        </Link>
+      </SettingSection>
+      <SettingSection>
+        <Link href="/mypage/changePhoto" passHref className="setting-link">
+          프로필사진 변경
+        </Link>
+      </SettingSection>
+      <SettingSection>
+        <Link href="/mypage/account" passHref className="setting-link">
+          계좌 정보
+        </Link>
+      </SettingSection>
+    </>
+  );
+};
+
+export default Settings;
+
+const SettingSection = styled.section`
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+  height: 55px;
+  padding: 0 21px;
+  border-bottom: 1px solid rgba(196, 196, 196, 0.2);
+  text-decoration: none;
+
+  .setting-link {
+    text-decoration: none;
+    color: #000;
+  }
+`;
