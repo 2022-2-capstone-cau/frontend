9eb39e21922afa50cf8bfe95c91c1854dcbcfcc2
hy57in <gwsyl22@cau.ac.kr>
2022-11-06 00:27:03 +0900

    feat: mypage  페이지

---
+++ src/pages/mypage/index.tsx
@@ -0,0 +1,59 @@
+import dayjs from 'dayjs';
+import styled from 'styled-components';
+
+import MyPageLayout from '@components/Layout/MyPageLayout';
+import MyProfile from '@components/MyProfile';
+import VisitedStore from '@components/VisitedStore';
+import { IMyOmakase, useMyOmakaseRecoilValue, useRefetchMyOmakases } from '@recoil/myOmakaseState';
+import { useFetchUserValue } from '@recoil/userState';
+
+const MyPage = () => {
+  useRefetchMyOmakases();
+
+  const { contents: userValue } = useFetchUserValue();
+  const {
+    contents: { omakases },
+  } = useMyOmakaseRecoilValue();
+
+  const replaceDate = (date: IMyOmakase['create_date']) => {
+    return dayjs(date).format('YYYY-MM-DD HH:mm:ss');
+  };
+
+  return (
+    <MyPageLayout>
+      <MyProfile userValue={userValue} />
+      <MyPagePage className="container">
+        <div className="store-list-title">
+          <span>{userValue.nickname}</span>님의 대출 목록
+        </div>
+        <div className="store-list-layout">
+          {omakases &&
+            omakases.map((omakase: IMyOmakase) => (
+              <VisitedStore
+                key={omakase.id}
+                id={omakase.id}
+                image={omakase.photo_url}
+                name={omakase.name}
+                date={replaceDate(omakase.create_date)}
+              />
+            ))}
+        </div>
+      </MyPagePage>
+    </MyPageLayout>
+  );
+};
+
+export default MyPage;
+
+const MyPagePage = styled.div`
+  .store-list-title {
+    ${({ theme }) => theme.fonts.subTitle1};
+    margin-bottom: 1.5rem;
+  }
+  .store-list-layout {
+    display: grid;
+    grid-auto-rows: auto;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 5px 15px;
+  }
+`;
